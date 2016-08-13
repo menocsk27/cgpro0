7a20ae35deba4fb706d6c623254aee18544b1844
Julián J. Méndez O <menoc.sk27@gmail.com>
2016-08-13 16:42:18 -0600

    primer gg

--- linesTemp/lines/main.c
+++ linesTemp/lines/main.c
@@ -6,8 +6,6 @@
 #define HSIZE 400
 #define VSIZE 400
 
-
-
 void line1 (int x0, int y0, int x1, int y1){
     int i, temp;
     long double m,b,x,y;
@@ -17,8 +15,8 @@ void line1 (int x0, int y0, int x1, int y1){
         m = ((long double)(y1 - y0)) / (x1 - x0);
         b = (long double)(y0 - (m * x0));
 
-        printf("m: %Lf ", m);
-        printf("b: %Lf \n", b);
+        //printf("m: %Lf ", m);
+        //printf("b: %Lf \n", b);
     }
     else {
         vertical = 1;
@@ -41,10 +39,10 @@ void line1 (int x0, int y0, int x1, int y1){
                 x = x0;
             }
 
-            printf("X: %Lf ", x);
-            printf("X: %f",round(x));
-            printf("Y: %i\n",i);
-            plot(round(x),i);
+            //printf("X: %Lf ", x);
+            //printf("X: %f",round(x));
+            //printf("Y: %i\n",i);
+            plot(((int)round(x)),i);
         }
     }
 
@@ -61,11 +59,11 @@ void line1 (int x0, int y0, int x1, int y1){
             else {
                 y = ((long double)(m * i + b));
 
-                printf("Y: %Lf ", y);
-                printf("Y: %f ", round(y));
-                printf("X: %i \n", i);
+                //printf("Y: %Lf ", y);
+                //printf("Y: %f ", round(y));
+                //printf("X: %i \n", i);
 
-                plot(i,round(y));
+                plot(i,((int)round(y)));
             }
         }
     }
@@ -77,7 +75,7 @@ void line2 (int x0, int y0, int x1, int y1){
 
     if ((x1 != x0)) {
         m = ((long double)(y1 - y0)) / (x1 - x0);
-        printf("m: %Lf \n", m);
+        //printf("m: %Lf \n", m);
     }
     else {
         vertical = 1;
@@ -93,10 +91,11 @@ void line2 (int x0, int y0, int x1, int y1){
         x = x0;
         //Cambiar variable independiente. x = (b+y)/m, delta = 1/m
         for (i = y0; i<=y1; i++){
-            printf("X: %Lf ", x);
-            printf("X: %f",round(x));
-            printf("Y: %i\n",i);
-            plot(round(x),i);
+            //printf("X: %Lf ", x);
+            //printf("X: %f",round(x));
+            //printf("Y: %i\n",i);
+
+            plot(((int)round(x)),i);
 
             if (vertical!=1) {
                 if (m != 0){
@@ -122,11 +121,11 @@ void line2 (int x0, int y0, int x1, int y1){
                 plot(x0,y0);
             }
             else {
-                printf("Y: %Lf ", y);
-                printf("Y: %f ", round(y));
-                printf("X: %i \n", i);
+                //printf("Y: %Lf ", y);
+                //printf("Y: %f ", round(y));
+                //printf("X: %i \n", i);
 
-                plot(i,round(y));
+                plot(i,((int)round(y)));
 
                 y += m;
             }
@@ -164,35 +163,192 @@ void line3 (int x0, int y0, int x1, int y1){
         }
     }
 }
-void line4 (int x0, int y0, int x1, int y1){
+void line4 (int x0, int y0, int x1, int y1){ //Bresenham (punto medio)
 
-}
+    int d2x,d2y,dx,dy,d,
+        Delta_N,Delta_NE,Delta_E,Delta_SE,
+        Delta_S,Delta_SW,Delta_W,Delta_NW,
+        xp,yp;
 
-void startScreen(){
+    xp = x0; yp = y0;
+    plot(xp,yp);
 
+    d2y = 2*(y1-y0);
+    d2x = 2*(x1-x0);
+    dy  = (y1-y0);
+    dx  = (x1-x0);
 
+    if (y1 > y0) { //1,2,3 o 4
+        if (x1 > x0) { //1 o 2
+            Delta_NE =  dy-dx;
+            if ( dy <= dx ){ //Octante 1
 
-    int gdriver = DETECT, gmode, errorcode;
-    initgraph(&gdriver, &gmode, "");
-    initwindow (400,400);
-    return 0;
-}
+                Delta_E  =  dy;
+                d = d2y-dy;
 
-void plot (int x, int y){
-    putpixel(x,getmaxy()-y,4);
-	return 0;
-}
+                while (xp <= x1){ //Avanza en x
+                    xp++;
+                    if (d<=0) {
+                        d += Delta_E;
+                    }
+                    else {
+                        yp++;
+                        d += Delta_NE;
+                    }
+                    plot(xp,yp);
+                }
+            }
+            else{ //Octante 2
 
+                Delta_N  = -dx;
+                d = dy -d2x;
 
-int max(int a, int b){
-    if (a < b){
-        return b;
-    }else{
-        return a;
+                while (yp <= y1){ //Avanza en y
+                    yp++;
+                    if (d<=0) {
+                        xp++;
+                        d += Delta_NE;
+                    }
+                    else {
+                        d += Delta_N;
+                    }
+                    plot(xp,yp);
+                }
+            }
+        }
+        else { //3 u 4
+            Delta_NW = -dy-dx;
+            if (dx >= -dy){ //Octante 3
+
+                Delta_N  = -dx;
+                d = -dy -d2x;
+
+                while (yp <= y1){ //Avanza en y
+                    yp++;
+                    if (d<=0) {
+                        d += Delta_N;
+                    }
+                    else {
+                        xp--;
+                        d += Delta_NW;
+                    }
+                    plot(xp,yp);
+                }
+            }
+            else{ //Octante 4
+                Delta_W  = -dy;
+                d = -d2y-dx ;
+
+                while (xp >= x1){ //Retrocede en x
+                    xp--;
+                    if (d<=0) {
+                        yp++;
+                        d += Delta_NW;
+                    }
+                    else {
+                        d += Delta_W;
+                    }
+                    plot(xp,yp);
+                }
+            }
+        }
+    }
+    else { //5,6,7 u 8
+        if (x1 < x0) { //5 o 6
+            Delta_SW = -dy+dx;
+            if (dx <= dy){ //Octante 5
+
+                Delta_W  = -dy   ;
+                d = -d2y+dx ;
+
+                while (xp >= x1){ //Retrocede en x
+                    xp--;
+                    if (d<=0) {
+                        d += Delta_W;
+                    }
+                    else {
+                        yp--;
+                        d += Delta_SW;
+                    }
+                    plot(xp,yp);
+
+                }
+            }
+            else{ //Octante 6
+
+                Delta_S  =  dx;
+                d = -dy +d2x;
+
+                while (yp >= y1){ //Retrocede en y
+                    yp--;
+                    if (d<=0) {
+                        xp--;
+                        d += Delta_SW;
+                    }
+                    else {
+                        d += Delta_S;
+                    }
+                    plot(xp,yp);
+
+                }
+            }
+        }
+        else { //7 u 8
+            Delta_SE =  dy+dx;
+            if (dx <= -dy){ //Octante 7
+
+                Delta_S  =  dx   ;
+                d =  dy +d2x;
+
+                while (yp >= y1){ //Retrocede en y
+                    yp--;
+                    if (d<=0) {
+                        d += Delta_S;
+                    }
+                    else {
+                        xp++;
+                        d += Delta_SE;
+                    }
+                    plot(xp,yp);
+
+                }
+            }
+            else{ //Octante 8
+
+                Delta_E  =  dy;
+                d =  d2y+dx;
+
+                while (xp <= x1){ //Avanza en x
+                    xp++;
+                    if (d<=0) {
+                        yp--;
+                        d += Delta_SE;
+                    }
+                    else {
+                        d += Delta_E;
+                    }
+                    plot(xp,yp);
+
+                }
+            }
+        }
     }
 }
 
+void startScreen(){
+    int gdriver = DETECT, gmode;
+    initgraph(&gdriver, &gmode, NULL);
+    closegraph();
 
+    initwindow (HSIZE,VSIZE);
+}
+void plot (int x, int y){
+    putpixel(x,getmaxy()-y,4);
+}
+int max(int a, int b){
+    if (a < b) { return b; }
+    else { return a; }
+}
 
 int main() {
     //El main debe asegurarse que
@@ -203,15 +359,23 @@ int main() {
 
     startScreen();
 
-    int x0 = 3;
-    int y0 = 4;
+    int x0 = 150;
+    int y0 = 150;
 
-    int x1 = 100;
-    int y1 = 70;
+    int x1 = 190;
+    int y1 = 190;
+
+    int x1s[8] = {150, 110, 110, 110, 150, 190, 190, 190};
+    int y1s[8] = {190, 190, 150, 110, 110, 110, 150, 190};
+
+    for (int i = 0; i<8; i++){
+        line1(x0     ,y0     ,x1s[i]     ,y1s[i]);
+        line2(x0     ,y0+100 ,x1s[i]     ,y1s[i]+100);
+        line3(x0+100 ,y0+100 ,x1s[i]+100 ,y1s[i]+100);
+        line4(x0+100 ,y0     ,x1s[i]+100 ,y1s[i]);
+    }
 
 
-    line3(x0,y0,x1,y1);
-    //printf("(%i,%i) a (%i,%i)\n", x0,y0,x1,y1);
     delay(6000);
     return 0;
 }
